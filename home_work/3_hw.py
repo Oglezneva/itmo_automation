82da2e18a89a2c8036ac5081cdbd113f47d64fb8
Oglezneva <oglezneva@yahoo.com>
2023-04-26 12:57:07 +0300

    first commit

--- home_work/3_hw.py
+++ home_work/3_hw.py
@@ -29,32 +29,31 @@
 # ЗАДАЧА 3
 
 
-# def season(x: int):
-#
-# match x:
-#     case 12, 1, 2:
-#         print('Зима')
-#     case 3, 4, 5:
-#         print('Весна')
-#     case 6, 7, 8:
-#         print('Лето')
-#     case 9, 10, 11:
-#         print('Осень')
-#
-#
-# season(7)
+def season(x):
+    match x:
+        case 12 | 1 | 2:
+            print('Зима')
+        case 3 | 4 | 5:
+            print('Весна')
+        case 6 | 7 | 8:
+            print('Лето')
+        case 9 | 10 | 11:
+            print('Осень')
+
+
+season(6)
 
 
 # ЗАДАЧА 4
 
 
-def task(a, b, c):
-    if a > 10 and b > 10 and c > 10:
-        print('Yes')
-    else:
-        print('No')
-
-
-task(21, 22, 23)
+# def task(a, b, c):
+#     if a > 10 and b > 10 and c > 10:
+#         print('Yes')
+#     else:
+#         print('No')
+#
+#
+# task(21, 22, 23)
 
 
